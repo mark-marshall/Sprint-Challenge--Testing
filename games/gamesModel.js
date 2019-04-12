35c1dfdcdd964b675b69fed7da57fbf84267792a
mark-marshall <mark.marshallgp@gmail.com>
2019-04-12 08:14:27 +0100

    [MVP] Set up structure for index.js, server,js, and gamesModel

---
+++ games/gamesModel.js
@@ -0,0 +1,26 @@
+const games = [
+  {
+    title: 'Pacman',
+    genre: 'Arcade',
+    releaseYear: 1980,
+  },
+  {
+    title: 'Space Invaders',
+    genre: 'Arcade',
+    releaseYear: 1978,
+  },
+  {
+    title: 'Snake',
+    genre: 'Video',
+    releaseYear: 1976,
+  },
+];
+
+const get = () => {
+  return users;
+};
+
+module.exports = {
+  games,
+  get,
+};
